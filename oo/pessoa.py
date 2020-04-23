6b8f0a88fd6fbb707137f90cc186287000f594ed
Alfonso AG <alfareiza@gmail.com>
2020-04-23 19:14:05 -0300

    Creados atributos de instancia nome and age

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,17 @@
-class ExemploPessoa:
-    pass
+class Pessoa:
+    def __init__(self, nome=None, age=None):
+        self.age = age
+        self.nome = nome
+
+    def cumprimentar(self):
+        return f'Hola {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa('Prueba Prueba',44) #Aqui estoy creando/construyendo un objeto p con la plantilla de la clase Pessoa, por ende se ejecuta el método init
+    print(p.cumprimentar())
+
+    print(p.nome) #Aqui acceso a un atributo del objeto P
+    p.nome = 'Alfonso' #Aqui modifico el atributo del objeto P
+    print(p.nome)  # Imprimo la modificación anterior
+
+    print(p.age) # Imprimo el atributo age del objeto p
